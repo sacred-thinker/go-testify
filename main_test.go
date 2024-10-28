785be21dbb32c905c02e5a299881251f8fe8e33b
sacred-thinker <yorik11@gmail.com>
2024-10-28 16:13:44 +0300

    second iteration

--- main_test.go
+++ main_test.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func TestMainHandleValidRequest(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Expected status code 200")
+	assert.NotEmpty(t, responseRecorder.Body.String(), "Expected non-empty response body")
+}
+
+func TestMainHandleInvalidCity(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=unknown", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Expected status code 400")
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String(), "Expected error message 'wrong city value")
+}
+
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+
 	totalCount := 4
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
@@ -16,18 +42,6 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	expectedBody := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
-	assert.Equal(t, expectedBody, responseRecorder.Body.String())
-
-	assert.NotEmpty(t, responseRecorder.Body.String())
-
-	cafesReturned := responseRecorder.Body.String()
-	cafesList := splitCafesString(cafesReturned)
-	assert.Equal(t, totalCount, len(cafesList))
-}
-
-func splitCafesString(cafesStr string) []string {
-	return strings.Split(cafesStr, ",")
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Expected 200 OK")
+	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), totalCount, "Expected all cafes")
 }
